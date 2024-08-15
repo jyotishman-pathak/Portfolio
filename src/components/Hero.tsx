ca552ae8023b89d875dd4daa0b575e86c20b0ea9
jyotishman-pathak <jyotishmanpathak11@gmail.com>
2024-08-15 18:49:43 +0530

    added the base

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div className="">
+      <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
+      
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
